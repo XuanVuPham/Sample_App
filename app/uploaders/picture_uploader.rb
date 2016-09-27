4ac312a752e63f204a5a80ddb8bb49fd37f39c56
phamxuanvu <xuanvupham96@gmail.com>
2016-09-27 15:05:04 +0700

    commit

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,13 +6,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
